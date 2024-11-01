b558e86547b8062cdd2ab11f8163fa5c7241b420
inferno0230 <mail@inferno0230.in>
2024-11-01 08:17:33 +0530

    build.sh: Bump llvm clang to 19.1.3
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- build.sh
+++ build.sh
@@ -13,8 +13,8 @@ KERNEL_PATH=$PWD
 ARCH=x86
 DEFCONFIG=custom_defconfig
 LLVM_DIR=.clang
-LLVM_URL="https://cdn.kernel.org/pub/tools/llvm/files/llvm-19.1.2-x86_64.tar.gz"
-LLVM_VER="llvm-19.1.2-x86_64"
+LLVM_VER="llvm-19.1.3-x86_64"
+LLVM_URL="https://cdn.kernel.org/pub/tools/llvm/files/$LLVM_VER.tar.gz"
 export PATH=$PWD/.clang/$LLVM_VER/bin:$PATH
 BUILD_CC="LLVM=1"
 
